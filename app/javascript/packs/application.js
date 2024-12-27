425639188bb3e2db0e25478de7035205d5027e99
kaneko <wakatsuki5656@gmail.com>
2024-12-27 11:37:10 +0900

    エラー解決のためのコミット

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap";
+import "@popperjs/core";
+import "./scripts";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
