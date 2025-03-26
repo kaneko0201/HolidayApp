2012ac773da4644e8ab4437e7827e6f2f6f71c44
kaneko <wakatsuki5656@gmail.com>
2025-03-26 20:22:34 +0900

    [fix]エラー修正

---
+++ config/initializers/autoload_fixes.rb
@@ -0,0 +1,2 @@
+Rails.autoloaders.main.ignore("#{Rails.root}/vendor/bundle/ruby/3.3.0/gems/actiontext-7.1.3/app/helpers")
+Rails.autoloaders.main.ignore("#{Rails.root}/vendor/bundle/ruby/3.3.0/gems/actiontext-7.1.3/app/models")
